0d4b1cf60fea889b33fe2a2626b9fd8ccd28c709
Marek Kundla <Marek.Kundla@gmail.com>
2023-04-24 08:45:51 +0200

    about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
+import Image from '../../assets/me-about.jpg'
 import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={Image} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>&lt;1 year</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small>100+ WorldWide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi veritatis assumenda eveniet magnam architecto id, deleniti officiis quos atque dolorem dicta cum, autem iste quas, harum vero aperiam. Fuga, quibusdam?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
