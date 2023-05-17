13a5be86cdd3920d3220525173c22a2ae524b8b2
Marek Kundla <Marek.Kundla@gmail.com>
2023-05-17 18:07:46 +0200

    portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crave',
+    github: 'https://github.com/MarekKundla/Crave',
+    demo: 'https://marekkundla.github.io/Crave/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Youtube clone',
+    github: 'https://github.com/MarekKundla/project_youtube_clone',
+    demo: 'https://marekkundla.github.io/Crave/'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='section'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>GitHub</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
